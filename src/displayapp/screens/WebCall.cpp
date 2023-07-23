47c37fb72813ef3b62fcd220812fa332f0d95b4a
Mantas Gudynas <mantas.gudynas@tracsis.com>
2023-07-23 17:53:54 +0100

    wip

--- src/displayapp/screens/WebCall.cpp
+++ src/displayapp/screens/WebCall.cpp
@@ -1,6 +1,9 @@
 #include "displayapp/screens/WebCall.h"
 #include "displayapp/InfiniTimeTheme.h"
 #include <components/ble/WebCallService.h>
+#include <numeric>
+#include <sstream>
+#include <cmath>
 
 using namespace Pinetime::Applications::Screens;
 
@@ -12,26 +15,24 @@ namespace {
 }
 
 WebCall::WebCall(Pinetime::Controllers::WebCallService& webCallService) : webCallService {webCallService} {
-  percent = lv_label_create(lv_scr_act(), nullptr);
-  lv_label_set_recolor(percent, true);
-  lv_label_set_long_mode(percent, LV_LABEL_LONG_BREAK);
-  lv_obj_set_width(percent, 240);
-  
-  lv_label_set_text_static(percent, "");
-  //lv_label_set_align(percent, LV_LABEL_ALIGN_CENTER);
-  lv_obj_align(percent, nullptr, LV_ALIGN_IN_TOP_MID, 0, 0);
-
-  btn_startStop = lv_btn_create(lv_scr_act(), nullptr);
-  btn_startStop->user_data = this;
-  lv_obj_set_height(btn_startStop, 50);
-  lv_obj_set_width(btn_startStop, 240);
-  lv_obj_set_event_cb(btn_startStop, btnStartStopEventHandler);
-  lv_obj_align(btn_startStop, nullptr, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
-  label_startStop = lv_label_create(btn_startStop, nullptr);
-  lv_label_set_text_static(label_startStop, "Make Web Call");
-
-  taskRefresh = lv_task_create(RefreshTaskCallback, 1000, LV_TASK_PRIO_MID, this);
-  Refresh();
+    lv_obj_t* act = lv_scr_act();
+    act->user_data = this;
+    lv_obj_set_event_cb(act, btnStartStopEventHandler);
+    
+    content_label = lv_label_create(lv_scr_act(), nullptr);
+    lv_label_set_recolor(content_label, true);
+    lv_label_set_long_mode(content_label, LV_LABEL_LONG_BREAK);
+    lv_obj_set_width(content_label, 240);
+    lv_label_set_text_static(content_label, "");
+    lv_obj_align(content_label, nullptr, LV_ALIGN_IN_TOP_MID, 0, 0);
+    
+    page_label = lv_label_create(lv_scr_act(), nullptr);
+    lv_obj_set_width(page_label, 240);
+    lv_label_set_text_static(page_label, "   ");
+    lv_obj_align(page_label, nullptr, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
+      
+    taskRefresh = lv_task_create(RefreshTaskCallback, 1000, LV_TASK_PRIO_MID, this);
+    MakeWebCall();
 }
 
 WebCall::~WebCall() {
@@ -39,19 +40,74 @@ WebCall::~WebCall() {
   lv_obj_clean(lv_scr_act());
 }
 
-void WebCall::OnButtonEvent(lv_obj_t* obj, lv_event_t event) {
-    if (event == LV_EVENT_CLICKED) {
-        lv_label_set_text_static(percent, "Making web call");
-        int const result = webCallService.MakeWebCall();
-        if (result > 0) {
-            lv_label_set_text_static(percent, "Web call failed");            
-        }
+void WebCall::OnButtonEvent(lv_obj_t* obj, lv_event_t event) {}
+
+void WebCall::MakeWebCall() {
+    lv_label_set_text_static(content_label, "Making web call");
+    int const result = webCallService.MakeWebCall();
+    if (result > 0) {
+        lv_label_set_text_static(content_label, "Web call failed");
     }
+}
+
+void WebCall::DrawItems() {
+    if (itemCount == 0) {
+        lv_label_set_text_static(content_label, "");
+        lv_label_set_text_static(page_label, "");
+        return;
+    }
+    
+    const uint8_t begin = currentPage * itemsPerPage;
+    uint8_t end = currentPage * itemsPerPage + itemsPerPage;
+    if (end > itemCount) end = itemCount;
+
+    const std::basic_string<char> &basicString = std::accumulate(
+            std::begin(values) + begin,
+            std::begin(values) + end,
+            std::string(),
+            [](std::string &acc, std::string &str) { return acc.empty() ? str : acc + "\n" + str; });
+
+    lv_label_set_text_fmt(content_label, "%s", basicString.c_str());
+    lv_label_set_text_fmt(page_label, "%d/%d", currentPage + 1, pageCount);
 }
 
 void WebCall::Refresh() {
     if (webCallService.getResponseReceived()) {
-        response = webCallService.getResponse();
-        lv_label_set_text_static(percent, response.data());
+        const std::string response = webCallService.getResponse();
+        webCallService.reset();
+
+        std::string str;
+        std::stringstream strStream(response);
+        int counter = 0;
+        while (getline(strStream, str, '\n')) {
+            if (counter == maxItems) break;
+            values[counter++] = str.length() > maxItemLength ? str.substr(0, maxItemLength) : str;            
+        }
+        
+        currentPage = 0;
+        itemCount = counter;
+        pageCount = itemCount == 0 ? 0 : ceil((float) itemCount / (float) itemsPerPage); 
+        
+        DrawItems();
     }
+}
+
+bool WebCall::OnTouchEvent(Pinetime::Applications::TouchEvents event) {
+    if (event == TouchEvents::SwipeLeft) {
+        if (currentPage > 0) {
+            currentPage--;
+            DrawItems();
+        }
+    } 
+    else if (event == TouchEvents::SwipeRight) {
+        if (currentPage < pageCount - 1) {
+            currentPage++;
+            DrawItems();
+        }
+    }
+    else if (event == TouchEvents::LongTap) {
+        MakeWebCall();
+    }
+    
+    return true;
 }
--- src/displayapp/screens/WebCall.h
+++ src/displayapp/screens/WebCall.h
@@ -10,27 +10,59 @@ namespace Pinetime {
     class WebCallService;
   }
 
-  namespace Applications {
-    namespace Screens {
-      class WebCall : public Screen {
+    namespace Applications::Screens {
+        class WebCall : public Screen {
         public:
-          WebCall(Pinetime::Controllers::WebCallService& webCallService);
-          ~WebCall() override;
-  
-          void Refresh() override;
-          void OnButtonEvent(lv_obj_t* object, lv_event_t event);
-  
+            WebCall(Pinetime::Controllers::WebCallService& webCallService);
+            ~WebCall() override;
+            
+            void Refresh() override;
+            void OnButtonEvent(lv_obj_t* acc, lv_event_t event);
+            bool OnTouchEvent(TouchEvents event) override;
         private:
-          Pinetime::Controllers::WebCallService& webCallService;
-          
-          lv_obj_t* percent;
-          lv_obj_t* btn_startStop;
-          lv_obj_t* label_startStop;
-          
-          lv_task_t* taskRefresh;
-          
-          std::string response {"NA"};
-      };
+            Pinetime::Controllers::WebCallService& webCallService;
+    
+            lv_obj_t* content_label;
+            lv_obj_t* page_label;
+    
+            lv_task_t* taskRefresh;
+            
+            uint8_t maxItems = 21;
+            uint8_t maxItemLength = 20;
+            uint8_t itemsPerPage = 7;
+            uint8_t pageCount = 0;
+            uint8_t currentPage = 0;
+            uint8_t itemCount = 0;
+            std::string values[21] {};
+            
+/*            uint8_t itemsPerPage = 7;
+            uint8_t pageCount = 3;
+            uint8_t currentPage = 0;
+            uint8_t itemCount = 18;
+            std::string values[21] {
+                "zero",
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine",
+                "ten",
+                "eleven",
+                "twelve",
+                "thirteen",
+                "fourteen",
+                "fifteen",
+                "sixteen",
+                "seventeen",
+            };
+*/
+            
+            void DrawItems();
+            void MakeWebCall();
+        };
     }
-  }
 }
